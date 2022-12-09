3aa07a23c75e48df2177c287539a3432aff1cee3
Jose De Anda <josdandar@gmail.com>
2022-12-08 22:14:20 -0800

    Assignment2

--- js/index.js
+++ js/index.js
@@ -1,42 +1,12 @@
-// console.log("Hello World");
-// var age = 100 ;
-// let userName = "Mike";
-// const URL = "rutaurlrandom";
+let productToBuy = prompt("Please type the product you want")
+let quantity = Number(prompt("Please type the quantity you want"))
+let priceToPay = Number(prompt("Please input a prrice"))
 
-// console.log(age, username, URL);
-// userName = "Edward";
-// console.log(userName, URL);
+function receiptDisplay(item, price, qty){
+    let total = 0
+    price = price * 0.16
+    total = price * quantity
+    document.write(`<h1>The total amount for the ${item} will be ${total} for a quantity of ${qty}<h1>`)
+}
 
-// let childrenNumber = 4;
-// let partnersName = "Andrea";
-// let userCity = "Oslo";
-// let jobTitle = "Software Developer";
-
-// document.write(`<h1> Jose has ${childrenNumber} childs with ${partnersName} and they both live in ${userCity} while he works as ${jobTitle} <h1>`)
-
-let netflixUser = "Tom Hanks"
-let netflixGenre = ["Comedy", "Action", "Horror"]
-let netflixFee = 7.00
-let netflixAccounts = 4
-let amazonUser = "Jeff"
-let amazonProduct = "Kindle"
-let amazonSeller = "Juan"
-let geoLocalization = "Helsinki"
-const URL = "https://media.giphy.com/media/1d7F9xyq6j7C1ojbC5/giphy.gif"
-let youtubeUser = "JessePvP"
-let viewCounter = 0
-let netflixMovie = "Bedtime Stories"
-let netflixMovie2 = "Your Name"
-let netflixMovie3 = "SpiritedAway"
-let amazonProduct2 = "Nasa Shirt"
-let amazonProduct3 = "Porsche 911 Lego Kit"
-let youtubeVideo1 = "https://www.youtube.com/watch?v=hPBdwv45kjo&list=PL9W8J8rFSgnXGvtuz2k-0KHA-ZQ5c_Q_K&index=1&ab_channel=ScenesBySevy"
-let youtubeVideo2 = "https://www.youtube.com/watch?v=mCdA4bJAGGk&ab_channel=sweetblue."
-let youtubeVideo3 = "https://www.youtube.com/watch?v=mCdA4bJAGGk&ab_channel=sweetblue."
-let youtubeVideo4 = "https://www.youtube.com/watch?v=mCdA4bJAGGk&ab_channel=sweetblue."
-
-document.write(`<div> <h1>Check out this amazing video</h1>
-<a href="${youtubeVideo1}">Click me</a>
-<p>Wanna see a cool movie from ${netflixUser}? Subscribe to Netflix for only ${netflixFee} and explore all our genres: ${netflixGenre[0] + " " +  netflixGenre[1]  + " " + netflixGenre[2]}<p> and create up to ${netflixAccounts} accounts!</div>`)
-
-document.write(`<div> <img src="${URL}"><h2><a href="${youtubeVideo2}">Click me click me!</a></h2></div>`)
+receiptDisplay(productToBuy, priceToPay, quantity)
